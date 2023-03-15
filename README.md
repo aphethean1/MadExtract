70e70a1bb06f9fcfd704fe8de8f1b9154d75bc4b
Aaron Phethean <aphethean@matatika.com>
2023-03-15 11:02:19 +0000

    Switch to lxml.html parsing, add total funding

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ Use xpath to extract all companies from mad (ML/AI/Data) landscape
 ## getting started
 
 1. create virtualenv `pyenv virtualenv 3.9.16 mad-extract && pyenv activate mad-extract`
-2. install dependencies `pip install elementpath`
+2. install dependencies `pip install lxml`
 3. `python3 extract.py`
 
 
@@ -12,4 +12,8 @@ Use xpath to extract all companies from mad (ML/AI/Data) landscape
 ## tools
 
 https://pypi.org/project/elementpath/
+
+## data source
+
+https://www.cbinsights.com/
 
--- extract.py
+++ extract.py
@@ -1,35 +1,46 @@
 import csv
 import os
 import sys
-from lxml import etree
+import lxml.html as lh
+from decimal import Decimal
+d = {
+    'K': 3,
+    'M': 6,
+    'B': 9
+}
+def text_to_num(text):
+    if text is None:
+        return None
+    if text[-1] in d:
+        num, magnitude = text[:-1], text[-1]
+        return Decimal(num) * 10 ** d[magnitude]
+    else:
+        return Decimal(text)
 
-parser = etree.XMLParser(attribute_defaults=False, load_dtd=True,   resolve_entities=True,remove_comments=False, ns_clean=True, recover=True)
 file = "./Landscape.html"
-data = etree.parse(file, parser=parser)
+doc = lh.parse(file)
 
-header = etree.XPath('''//h2[contains(@class, 'text-lg font-bold')]''')
-section = etree.XPath('''//li[contains(@class, 'relative')]''')
-#category = etree.XPath('''//h3[contains(@class, 'border-fmgold dark:border-yellow-400 text-fmgold dark:text-yellow-400 sticky top-[54px] z-10 bg-white pb-3 text-2xl dark:bg-slate-900')]''')
-#categories = [x.text for x in category(data)]
-#print(categories)
+headers = doc.xpath('''//h2[contains(@class, 'text-lg font-bold')]''')
+sections = doc.xpath('''//li[contains(@class, 'relative')]''')
 
 count = 0
 cards = []
-for section_element in section(data):
-    # [etree.dump(x) for x in section_element.findall('''./h3[1]''')]
+for section_element in sections:
     category = section_element.find('''./h3[1]''').text if section_element.find('''./h3[1]''') is not None else None
     if category is None:
         continue
     card_elements = section_element.find('ul').findall('''./li''')
-    #print(category,":",len(card_elements))
     if card_elements is not None:
         for element in card_elements:
-            company = element.find('./div[1]/div[2]/h2').text if element.find('./div[1]/div[2]/h2') is not None else None
-            country = element.find('./div[1]/div[2]/span').text if element.find('./div[1]/div[2]/span') is not None else None
-            desc = element.find('./p').text if element.find('./p') is not None else None
+            company = element.find('./div[1]/div[2]/h2').text_content() if element.find('./div[1]/div[2]/h2') is not None else None
+            country = element.find('./div[1]/div[2]/span').text_content() if element.find('./div[1]/div[2]/span') is not None else None
+            funding = element.find('./div[2]/span[2]').text_content() if element.find('./div[2]/span[2]') is not None else None
+            funding = funding.replace('Total Funding: ', '') if funding is not None else None
+            funding_num = text_to_num(funding.replace('$','')) if funding is not None else None
+            desc = element.find('./p').text_content() if element.find('./p') is not None else None
             if company:
-                cards.append([category, company, country, desc])
+                cards.append([category, company, country, funding, funding_num, desc])
 
 writer = csv.writer(sys.stdout, lineterminator=os.linesep)
-writer.writerow(['Category','Company','Country','Description'])
+writer.writerow(['Category','Company','Country','Total Funding','Funding Numeric','Description'])
 writer.writerows(cards)
